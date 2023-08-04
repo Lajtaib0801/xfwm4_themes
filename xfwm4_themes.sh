20a477cd6607530fb6dd13e0931b59c7d98b34fa
Lajtaib0801 <62424833+Lajtaib0801@users.noreply.github.com>
2023-08-04 17:01:35 +0200

    Add files via upload

---
+++ xfwm4_themes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install wget xfce4-dev-tools -y
+mkdir ~/xfwm4themes
+cd ~/xfwm4themes
+
+wget https://git.xfce.org/archive/xfwm4-themes/snapshot/xfwm4-themes-4.10.0.tar.gz
+
+tar -xvf xfwm4-themes-4.10.0.tar.gz
+cd xfwm4-themes-4.10.0
+sudo ./autogen.sh
+make
+sudo ./configure
+sudo make install
